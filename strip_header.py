f31241a9be8ed7c6c2ca85a23d73fc27e8be1a14
Ryan May <rmay31@gmail.com>
2015-12-11 16:19:41 -0700

    Handle creating necessary directories.
    Also add a bunch of logging.

--- strip_header.py
+++ strip_header.py
@@ -3,8 +3,29 @@
 # Distributed under the terms of the MIT License.
 # SPDX-License-Identifier: MIT
 
+import os
+import os.path
 import re
 import sys
+
+
+def init_logger():
+    import logging
+    import logging.handlers
+    import socket
+
+    # Set the global logger
+    global logger
+    logger = logging.getLogger('HeaderStripHandler')
+
+    # Send logs to LDM's log if possible, otherwise send to stderr.
+    try:
+        handler = logging.handlers.SysLogHandler(address='/dev/log', facility='local0')
+    except socket.error:
+        handler = logging.StreamHandler()
+
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
 
 
 # Find WMO header and remove
@@ -24,17 +45,31 @@ def remove_footer(block):
     else:
         return block
 
-# Read first block and remove header (no guarantee read() gets all data)
-block = remove_header(sys.stdin.read())
-blocks = []
+try:
+    init_logger()
+    logger.debug('Started script.')
 
-# While we keep getting data, try to remove the footer. Easier than trying to remove footer
-# only for last block.
-while block:
-    blocks.append(remove_footer(block))
-    block = sys.stdin.read()
+    # Read first block and remove header (no guarantee read() gets all data)
+    block = remove_header(sys.stdin.read())
+    blocks = []
 
-# Write the data out
-with open(sys.argv[1], 'wb') as f:
-    for b in blocks:
-        f.write(b)
+    # While we keep getting data, try to remove the footer. Easier than trying to remove footer
+    # only for last block.
+    while block:
+        blocks.append(remove_footer(block))
+        block = sys.stdin.read()
+
+    # Make sure directory exists
+    target_file = sys.argv[1]
+    target_dir = os.path.dirname(target_file)
+    logger.debug('Writing to %s in %s', target_file, target_dir)
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+
+    # Write the data out
+    with open(target_file, 'wb') as f:
+        for b in blocks:
+            f.write(b)
+    logger.info('Successfully wrote to: %s', target_file)
+except Exception as e:
+    logger.exception("Exception!")
